ae4be6079fe08f650fa7bf6103185dd56435946e
Evan MULUMBA <evans.mllb@gmail.com>
2021-04-30 13:07:26 +0200

    mobile blogr

---
+++ blogr-landing-page-main/script.js
@@ -0,0 +1,87 @@
+let menu_display = false;
+let product_display = false;
+let company_display = false;
+let connect_display = false;
+
+function show_menu(){
+    document.getElementById("menu").style.display="flex";
+    document.getElementById("logo-menu").src="images/icon-close.svg";
+}
+function hide_menu(){
+    document.getElementById("menu").style.display="none";
+    document.getElementById("logo-menu").src="images/icon-hamburger.svg";
+}
+function show_section(id){
+    document.getElementById(id).style.display="flex";
+}
+function hide_section(id){
+    document.getElementById(id).style.display="none";
+}
+function upright(id){
+    document.getElementById(id).style.transform= 'scaleY(1)';
+}
+function upside(id){
+    document.getElementById(id).style.transform= 'scaleY(-1)';
+}
+function light_font(id){
+    document.getElementById(id).style.color = 'hsl(208, 19%, 54%)';
+}
+function dark_font(id){
+    document.getElementById(id).style.color = 'hsl(208, 39%, 34%)';
+}
+
+
+
+function menu_action(id){
+    if(id == 1){
+        if (product_display) {
+            hide_section('product-section');
+            upright("product-arrow");
+            dark_font('product-header');
+        }
+        else {
+            show_section('product-section');
+            upside("product-arrow");
+            light_font('product-header')
+        }
+        product_display = !product_display;
+    }
+    else if(id == 2){
+        if (company_display) {
+            hide_section('company-section');
+            upright("company-arrow")
+            dark_font('company-header')
+        }
+        else {
+            show_section('company-section');
+            upside("company-arrow");
+            light_font('company-header')
+
+        }
+        company_display = !company_display;
+    }
+    else if(id == 3){
+        if (connect_display) {
+            hide_section('connect-section');
+            upright("connect-arrow")
+            dark_font('connect-header')
+        }
+        else {
+            show_section('connect-section');
+            upside("connect-arrow");
+            light_font('connect-header')
+        }
+        connect_display = !connect_display;
+
+    }
+    else
+    {
+        if (menu_display == false) {
+            show_menu();
+            menu_display = true;
+        } else {
+            hide_menu();
+            menu_display = false;
+        }
+    }
+}
